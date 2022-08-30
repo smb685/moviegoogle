a55e66a2c4c3d81ebf9ca76cb94d67aaa6553d40
Sylvester Brandon <sylvester.brandon25@gmail.com>
2022-08-30 15:00:06 -0500

    Update LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+// this imports the { loginUser } from '../utils/API'
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
